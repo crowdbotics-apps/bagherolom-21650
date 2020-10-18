6e883c642a717ecc1aa4832c371d1dddf309081d
Crowdbotics <team@crowdbotics.com>
2020-10-18 05:39:43 +0000

    Update models

--- backend/bagherolom_21650/wsgi.py
+++ backend/bagherolom_21650/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bagherolom_21650.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bagherolom_21650.settings")
 
 application = get_wsgi_application()
